56f09962686fd534a7aefb03e068468d9dc2cecf
unfaiyted <unfaiyted@gmail.com>
2018-03-26 21:13:42 -0500

    page design css structure

--- src/api.js
+++ src/api.js
@@ -18,5 +18,16 @@ module.exports = {
     deleteMovie: (id) => {
     // delete existing movies
 
+    },
+    getMovieDBData: (external_id) => {
+        const data= {
+            "api_key": '0c3cf3e726b6d3566fbe38b74c239e5b',
+            "langauge": 'en-US',
+            "external_source": 'imdb_id'
+        };
+
+        return fetch(`https://api.themoviedb.org/3/find/${external_id}?api_key=${data.api_key}&language=${data.langauge}&external_source=${data.external_source}`)
+            .then(response => response.json());
+
     }
 };
--- src/index.js
+++ src/index.js
@@ -7,28 +7,65 @@
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const $ = require('jquery');
+const {getMovies, getMovieDBData} = require('./api.js');
 const {sayHello} = require('./hello.js');
 
 sayHello('Dane');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-
-      $('<div class="movie">').append(
-          $('<div class="movie-id">').text(id),
-          $('<div class="movie-title">').text(title),
-          $('<div class="movie-rating">').text(rating)
-      ).appendTo('#moviesList');
+    //console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+//    console.log(`id#${id} - ${title} - rating: ${rating}`);
 
 
-      $('.loading-container').hide();
+        $('<div class="movie p-2 m-2">').append(
+            $('<div class="movie-img">').append(
+                $(`<img src="images/placeholder.png" id="img-${id}" class="img-fluid">`)),
+            $('<div class="movie-title">').text(title),
+            // $('<div class="movie-rating">').text(rating),
+            $('<div class="movie-database">').append(
+                $(`<button type="submit" id="update-movie-${id}" value="${id}">`).text("Get Data"),
+            )).appendTo('#moviesList');
 
 
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+        $(`#update-movie-${id}`).click(function (e) {
+            e.preventDefault();
+            $('.loading-container').show();
+            console.log($(this).val());
+
+            const movieId = $(this).val();
+
+            $(this).off('click');
+
+            setTimeout(function () {
+                getMovieDBData(movieId).then((movies) => {
+                    console.log('Here are all the movies:');
+                    console.log(movies.movie_results[0]);
+
+                    const movie = movies.movie_results[0];
+
+                    $(`#img-${id}`).attr('src',`https://image.tmdb.org/t/p/w500/${movie.poster_path}`);
+                    $('html, body').css('background-image',`url('https://image.tmdb.org/t/p/original/${movie.backdrop_path}')`);
+
+                }).catch((error) => {
+                    alert('Oh no! Something went wrong.\nCheck the console for details.');
+                    console.log(error);
+                });
+
+                $(this).on('click');
+
+                $('.loading-container').hide();
+            }, 3000);
+
+        });
+
+        $('.loading-container').hide();
+
+    });
+
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
+
